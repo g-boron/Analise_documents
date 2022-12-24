fa1d8eb79afb505c09cf68c1066523b9255a6636
georg00 <grzesiekb2013@gmail.com>
2022-12-24 11:13:50 +0100

    Added argv for statistics and counting words

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ import getopt
 def main(argv):
     arg_download = ''
     arg_keyword = ''
-    arg_collect = ''
     arg_stats = ''
     arg_count_word = ''
-    arg_help = f'{argv[0]} -d | --download <number of papers> -k | --keyword <keyword> -c | --collect <y/n> -s | --stats <y/n> -w | --count_word <word>'
+    arg_help = f'{argv[0]} -d | --download <number of papers> -k | --keyword <keyword> -s | --stats <y/n> -w | --count_words <word>'
 
     try:
-        opts, args = getopt.getopt(argv[1:], 'hd:k:c:s:w:', ['help', 'download=', 'keyword=', 'collect=', 'stats=', 'count_word='])
+        opts, args = getopt.getopt(argv[1:], 'hd:k:s:w:', ['help', 'download=', 'keyword=', 'stats=', 'count_word='])
     except:
         print(arg_help)
         sys.exit(2)
@@ -27,21 +26,33 @@ def main(argv):
             arg_keyword = arg
         elif opt in ('-d', '--download'):
             arg_download = arg
-        elif opt in ('-c', '--collect'):
-            arg_collect = arg
         elif opt in ('-s', '--stats'):
             arg_stats = arg
         elif opt in ('-w', '--count_word'):
             arg_count_word = arg
 
-    print('download', arg_download)
-    print('collect', arg_collect)
-    print('stats', arg_stats)
-    print('count_word', arg_count_word)
-
     if arg_download != '' and arg_keyword != '':
-        titles, urls, ids = papers.collect_data(arg_download, arg_keyword)
+        ids = papers.collect_data(arg_download, arg_keyword)
         papers.download_pdf(ids)
+
+    if arg_stats == 'y':
+        pdfs, words, chars, chars_wo_spaces = calculation.get_chars_stats()
+        df = data_analysis.create_dataframe(File = pdfs, Words = words, Chars = chars, Chars_without_spaces = chars_wo_spaces)
+        with open('keyword.txt', 'r') as f:
+            content = f.read()
+            finded_words = calculation.count_words(content)
+            df['Finded_keywords'] = finded_words
+            print(df)
+
+    if arg_count_word != '':
+        finded_words = calculation.count_words(arg_count_word)
+        try:
+            df = df.drop('Finded_keywords', axis=1)
+            df['Finded_words'] = finded_words
+            print(df)
+        except:
+            df = data_analysis.create_dataframe(Finded_words = finded_words)
+            print(df)
 
     #titles, urls, ids = papers.collect_data()
     #papers.download_pdf(ids)
--- papers.py
+++ papers.py
@@ -2,6 +2,7 @@ import arxiv
 import re
 from progress.bar import Bar
 import os
+from time import time
 
 
 def fetch_results(amount, keyword):
@@ -18,24 +19,24 @@ def collect_data(amount, keyword):
     print()
     print('Collecting data..')
 
+    with open('keyword.txt', 'w') as f:
+        f.write(keyword)
+
     papers = fetch_results(amount, keyword)
-    titles = []
-    urls = []
     ids = []
 
     with Bar('Processing...', max=int(amount), suffix='%(percent)d%%') as bar:
         for _, result in enumerate(papers, 1):
             slashs = [m.start() for m in re.finditer('/', result.entry_id)]
             paper_id = result.entry_id[slashs[-1]+1:]
-            titles.append(result.title)
-            urls.append(result.pdf_url)
             ids.append(paper_id)
             bar.next()
 
-    return titles, urls, ids
+    return ids
 
 
 def download_pdf(ids):
+    start_time = time()
     print()
     print('Downloading papers..')
 
@@ -43,5 +44,7 @@ def download_pdf(ids):
     with Bar('Processing...', max=len(ids), suffix='%(percent)d%%') as bar:
         for idx, paper_id in enumerate(ids, 1):
             paper = next(arxiv.Search(id_list=[paper_id]).results())
-            paper.download_pdf(dirpath='./pdfs', filename=f'paper{idx}.pdf')
+            paper.download_pdf(dirpath='./pdfs')
             bar.next()
+
+    print(f'Time spent: {time()-start_time}s')
